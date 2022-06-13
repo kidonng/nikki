1973d50cb31be060ba6f337e5dae786be36d6514
Kid <hi@xuann.wang>
2022-06-13 17:38:45 +0800

    Don't open gallery directly

--- script.ts
+++ script.ts
@@ -6,10 +6,8 @@ import type {} from 'dayjs/plugin/dayOfYear.d.ts'
 dayjs.extend(dayOfYear)
 
 const today = dayjs().dayOfYear()
-const getIndex = () => {
-    const hash = parseInt(location.hash.slice(1))
-    return (Number.isNaN(hash) ? today : Math.min(Math.max(1, hash), 365)) - 1
-}
+const date = (day: number) => dayjs().dayOfYear(day).format('MMM D')
+const available = (day: number) => day <= 126 || day >= 151
 
 // https://avif.io/blog/tutorials/css/#avifsupportdetectionscript
 const image = new Image()
@@ -20,32 +18,58 @@ const format = await new Promise<string>((resolve) => {
         'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A='
 })
 
-const pswp = new PhotoSwipe({
-    dataSource: [...Array(365).keys()].map(
-        (i) =>
-            (i <= 125 || i >= 150
-                ? {
-                      src: `/${i + 1}.${format}`,
-                      width: 1080,
-                      height: 1350,
-                  }
-                : {
-                      html: `
+document.querySelector<HTMLAnchorElement>('#link')!.hash = String(today)
+const img = document.querySelector<HTMLImageElement>('#img')!
+img.src = `/${today}.${format}`
+img.alt = date(today)
+
+const dataSource = [...Array(365).keys()].map((index) => {
+    const day = index + 1
+    return (
+        available(day)
+            ? {
+                  src: `/${day}.${format}`,
+                  width: 1080,
+                  height: 1350,
+                  alt: date(day),
+              }
+            : {
+                  html: `
                         <div style="display: flex; height: 100%; justify-content: center; align-items: center;">
                             <span style="font-size: 5rem; filter: invert(1);">🔜</span>
                         </div>
                       `,
-                  }) as SlideData
-    ),
-    index: getIndex(),
-    bgOpacity: 1,
+              }
+    ) as SlideData
 })
-window.addEventListener('hashchange', () => pswp.goTo(getIndex()))
 
+let pswp: PhotoSwipe
 const { title } = document
-pswp.on('change', () => {
-    const curr = pswp.currIndex + 1
-    location.hash = curr === today ? '' : String(curr)
-    document.title = `${dayjs().dayOfYear(curr).format('MMM D')} - ${title}`
-})
-pswp.init()
+
+const init = (index: number) => {
+    pswp = new PhotoSwipe({
+        dataSource,
+        index,
+    })
+    pswp.on('change', () => {
+        const day = pswp.currIndex + 1
+        location.hash = String(day)
+        document.title = `${date(day)} - ${title}`
+    })
+    pswp.on('close', () => {
+        location.hash = ''
+        document.title = title
+    })
+    pswp.init()
+}
+
+const hash = () => {
+    const day = parseInt(location.hash.slice(1))
+    if (!Number.isNaN(day) && day >= 1 && day <= 365) {
+        const index = day - 1
+        if (!pswp || pswp.isDestroying) init(index)
+        else pswp.goTo(index)
+    }
+}
+hash()
+window.addEventListener('hashchange', hash)
